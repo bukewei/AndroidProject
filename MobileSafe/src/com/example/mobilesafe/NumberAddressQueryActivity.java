f5c2e236d428abc34c5e2573af22548b6f711511
bukewei <709703867@qq.com>
2016-01-01 15:42:09 +0800

    输入框的抖动、震动效果

--- MobileSafe/src/com/example/mobilesafe/NumberAddressQueryActivity.java
+++ MobileSafe/src/com/example/mobilesafe/NumberAddressQueryActivity.java
@@ -5,10 +5,13 @@ import com.example.mobilesafe.db.dao.NumberAddressQueryUtils;
 import android.app.Activity;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,11 +21,16 @@ public class NumberAddressQueryActivity extends Activity {
 	private EditText et_phone;
 	private TextView tv_result;
 	
+	//系统的震动服务
+	private Vibrator vibrator;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自动生成的方法存根
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_number_addres_query);
+		
+		vibrator=(Vibrator) getSystemService(VIBRATOR_SERVICE);
 		
 		et_phone=(EditText) findViewById(R.id.et_phone);
 		tv_result=(TextView) findViewById(R.id.tv_result);
@@ -34,7 +42,16 @@ public class NumberAddressQueryActivity extends Activity {
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				// TODO 自动生成的方法存根
-				System.out.println("文本发生变化了！！！");
+	//			System.out.println("文本发生变化了！！！");
+				System.out.println(s.toString());
+				if(s != null && s.length() >= 3){
+					//查询数据库，并显示结果
+					String address=NumberAddressQueryUtils.queryNumber(s.toString());
+					tv_result.setText(address);
+				}else {
+					tv_result.setText(s.toString());
+				}
+				
 			}
 			/**
 			 * 当文本发生变化之前回调
@@ -63,6 +80,15 @@ public class NumberAddressQueryActivity extends Activity {
 		String phone=et_phone.getText().toString().trim();
 		if(TextUtils.isEmpty(phone)){
 			Toast.makeText(this,"要查询的号码不能为空",Toast.LENGTH_SHORT).show();
+			//文本框抖动
+			Animation shake=AnimationUtils.loadAnimation(this,R.anim.shake);
+			et_phone.startAnimation(shake);
+			//手机震动
+	//		vibrator.vibrate(2000);//震动多长时间
+	//		long[] pattern={100,200,200,300,300,400,100,500};
+			long[] pattern={100,100};
+			//-1：不重复 0：循环震动     1：从数组的第二个开始
+			vibrator.vibrate(pattern, -1);
 			
 		}else{
 			//开始查询
--- MobileSafe/src/com/example/mobilesafe/db/dao/NumberAddressQueryUtils.java
+++ MobileSafe/src/com/example/mobilesafe/db/dao/NumberAddressQueryUtils.java
@@ -2,6 +2,7 @@ package com.example.mobilesafe.db.dao;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
 
 public class NumberAddressQueryUtils {
 	
@@ -16,14 +17,66 @@ public class NumberAddressQueryUtils {
 		String address=number;
 		// path 把address.db这个数据库拷贝到data/data/《包名》/files/address.db
 		SQLiteDatabase database=SQLiteDatabase.openDatabase(path, null,SQLiteDatabase.OPEN_READONLY);
-		String  sql="SELECT location FROM data2 WHERE id=(SELECT outkey FROM data1 WHERE id=?)";
-		//number.substring(0,7);截取文本的第一位到第六位
-		Cursor cursor=database.rawQuery(sql, new String[]{number.substring(0,7)});
-		while(cursor.moveToNext()){
-			String location=cursor.getString(0);
-			address=location;
+		
+		//手机号码的正则表达式
+		if(number.matches("^1[34568]\\d{9}$")){
+			//是手机号码
+			String  sql="SELECT location FROM data2 WHERE id=(SELECT outkey FROM data1 WHERE id=?)";
+			//number.substring(0,7);截取文本的第一位到第六位
+			Cursor cursor=database.rawQuery(sql, new String[]{number.substring(0,7)});
+			while(cursor.moveToNext()){
+				String location=cursor.getString(0);
+				address=location;
+			}
+			cursor.close();
+			
+		}else{
+			//其他号码
+			switch (number.length()) {
+			case 3:
+				// 110
+				address = "匪警号码";
+				break;
+			case 4:
+				// 5554
+				address = "模拟器";
+				break;
+			case 5:
+				// 10086
+				address = "客服电话";
+				break;
+			case 7:
+				//
+				address = "本地号码";
+				break;
+
+			case 8:
+				address = "本地号码";
+				break;
+			default:
+				//长度大于10  0开头
+				if(number.length() > 10 && number.startsWith("0")){
+					String sql="SELECT location FROM data2 WHERE area=?";
+					String location="";
+					Cursor cursor=database.rawQuery(sql, new String[]{number.substring(1,3)});
+					while(cursor.moveToNext()){
+						location=cursor.getString(0);
+					}
+					
+					if(TextUtils.isEmpty(location)){
+						cursor=database.rawQuery(sql, new String[]{number.substring(1,4)});
+						while(cursor.moveToNext()){
+							location=cursor.getString(0);
+						}
+					}
+					cursor.close();
+					address=location.substring(0,location.length()-2);
+					
+				}
+				break;
+			}
 		}
-		cursor.close();
+		
 		
 		return address;
 	}
