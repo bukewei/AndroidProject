f86e5e976c47a208eedfbc3d3429c1f3df4dee9f
bukewei <709703867@qq.com>
2016-01-06 15:29:55 +0800

    电话拦截

--- MobileSafe/src/com/example/mobilesafe/service/CallSSmsSafeService.java
+++ MobileSafe/src/com/example/mobilesafe/service/CallSSmsSafeService.java
@@ -1,5 +1,9 @@
 package com.example.mobilesafe.service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import com.android.internal.telephony.ITelephony;
 import com.example.mobilesafe.db.dao.BlackNumberDao;
 
 import android.app.Service;
@@ -8,7 +12,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class CallSSmsSafeService extends Service {
@@ -16,6 +22,8 @@ public class CallSSmsSafeService extends Service {
 	private static final String TAG="CallSSmsSafeService";
 	private BlackNumberDao dao;
 	private InnerSmsReceiver receiver;
+	private TelephonyManager tm;
+	private MyListener listener;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -67,6 +75,11 @@ public class CallSSmsSafeService extends Service {
 		super.onCreate();
 		// 服务被创建 进行初始化
 		dao=new BlackNumberDao(this);
+		//得到电话管理服务
+		tm=(TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		listener=new MyListener();
+		//监听电话状态
+		tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 		receiver=new InnerSmsReceiver();
 		//代码注册短信广播接收者
 		IntentFilter filter=new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
@@ -83,5 +96,51 @@ public class CallSSmsSafeService extends Service {
 		super.onDestroy();
 		unregisterReceiver(receiver);
 		receiver=null;
+		tm.listen(listener, PhoneStateListener.LISTEN_NONE);
 	}
+	/**
+	 *  电话状态监听器
+	 */
+	private class MyListener extends PhoneStateListener{
+		/**
+		 * 呼叫状态改变
+		 */
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			// TODO 自动生成的方法存根
+			switch (state) {
+			case TelephonyManager.CALL_STATE_RINGING://响铃状态
+				String result=dao.findMode(incomingNumber);//查询拦截模式
+				if("1".equals(result) || "0".equals(result)){
+					Log.i(TAG, "黑名单号码，挂断电话");
+					endCall();
+				}
+				break;
+
+			default:
+				break;
+			}
+			
+			super.onCallStateChanged(state, incomingNumber);
+		}
+
+	}
+	
+	public void endCall() {
+		//特殊方法调用系统源码
+		try {
+			
+			//加载servicemanager的字节码
+			Class clazz=CallSSmsSafeService.class.getClassLoader().loadClass("android.os.ServiceManager");
+			Method method=clazz.getDeclaredMethod("getService",String.class);
+			IBinder ibinder=(IBinder) method.invoke(null,TELEPHONY_SERVICE);
+			ITelephony.Stub.asInterface(ibinder).endCall();
+			
+		} catch (Exception e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} 		
+		
+	}
+	
 }
